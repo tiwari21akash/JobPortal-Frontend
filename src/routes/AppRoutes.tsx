bc6d82c534482d51e02093f11db01b8f6aecd266
tiwari21akash <tiwari21akash@gmail.com>
2025-08-16 00:16:25 +0530

    removed unused file

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,25 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import FindJobs from "../pages/FindJobs";
 import About from "../pages/About";
-import Learning from "../pages/Learning";
 import Home from "../pages/Home";
 import ViewJob from "../pages/ViewJob";
 import AdminPostJob from '../pages/AdminPostJob';
 import AdminJobList from "../pages/AdminJobList";
 import LoginCard from "../components/LoginCard";
 import RegisterCard from "../components/RegisterCard";
-import LearningContent from "../pages/LearningContent";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/find-jobs" element={<FindJobs />} />
-      <Route path="/learning" element={<Learning />} />
-      <Route path="/learning/:id" element={<LearningContent />} />
+      <Route path="/jobs/:jobId" element={<ViewJob />} />
       <Route path="/login" element={<LoginCard />} />
       <Route path="/register" element={<RegisterCard />} />
-      <Route path="/jobs/:jobId" element={<ViewJob />} />
       <Route path="/admin/post-job" element={<AdminPostJob />} />
       <Route path="/admin/jobs" element={<AdminJobList />} />
       <Route path="/about" element={<About />} />
